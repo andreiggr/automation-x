c798a3bf2a13d85f41d6d5a5477dabb091868217
andreiggr <andrei.ggr@gmail.com>
2019-12-01 20:34:32 +0200

    minor edit product page

--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -10,104 +10,73 @@ import PropTypes from 'prop-types';
 
 const phoneImg = require('../../assets/phone.png');
 
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3)
-    },
-    categoryTitle: {
-        fontSize: 22,
-        marginBottom: 29,
-        marginTop: 29
-    },
-    content: {
-        marginTop: theme.spacing(2)
-    },
-    pagination: {
-        marginTop: theme.spacing(3),
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end'
-    },
-    backButton: {
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center'
-    },
-    contentBar: {
-        display: 'flex',
-        flexDirection: 'row',
-        height: '42px',
-        justifyContent: 'space-between'
-    }
+const useStyles = makeStyles((theme) => ({
+	root: {
+		padding: theme.spacing(3)
+	},
+	categoryTitle: {
+		fontSize: 22,
+		marginBottom: 29,
+		marginTop: 29
+	},
+	content: {
+		marginTop: theme.spacing(2)
+	},
+	pagination: {
+		marginTop: theme.spacing(3),
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end'
+	},
+	backButton: {
+		cursor: 'pointer',
+		display: 'flex',
+		alignItems: 'center'
+	},
+	contentBar: {
+		display: 'flex',
+		flexDirection: 'row',
+		height: '42px',
+		justifyContent: 'space-between'
+	}
 }));
 
-const gitURL = 'https://github.com/mitesh77/Best-Flutter-UI-Templates'
-
+const gitURL = 'https://github.com/mitesh77/Best-Flutter-UI-Templates';
 
 const Product = () => {
-    const classes = useStyles();
-    return (
-        <FirebaseContext.Consumer>
-            {firebase => {
-                return (
-                    <div className={classes.root}>
-                        <ProductsToolbar />
-                        <div className={classes.content}>
-                            <Grid
-                                container
-                                direction="row"
-                                spacing={3}
-                                xs={12}
-                            >
-                                <Grid
-                                    container
-                                    direction="column"
-                                    xs={7}
-                                >
-                                    <div className={classes.contentBar}>
-                                        <div className={classes.backButton}>
-                                            <ArrowBackIosIcon fontSize="small" />
-                                            <Typography className={classes.categoryTitle}>Project Name</Typography>
-                                        </div>
-                                        <Button
-                                            color="primary"
-                                            variant="contained"
-                                        >
-                                            Run on Emulator
-                                </Button>
-                                    </div>
-                                    <GitCard git={gitURL} />
-                                    <ContentCard git={gitURL} />
-                                </Grid>
-                                <Grid
-                                    alignContent="center"
-                                    container
-                                    direction="column"
-                                    xs={5}
-                                >
-                                    <div style={{}}>
-                                        <img
-                                            src={phoneImg}
-                                            style={{ maxHeight: '900px' }}
-                                        />
-                                    </div>
-                                </Grid>
-                            </Grid>
-                        </div>
-                    </div>
-                );
-            }
-            }
-        </FirebaseContext.Consumer>
-    )
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<ProductsToolbar />
+			<div className={classes.content}>
+				<Grid container direction="row" spacing={3} xs={12}>
+					<Grid container direction="column" xs={7}>
+						<div className={classes.contentBar}>
+							<div className={classes.backButton}>
+								<ArrowBackIosIcon fontSize="small" />
+								<Typography className={classes.categoryTitle}>Project Name</Typography>
+							</div>
+							<Button color="primary" variant="contained">
+								Run on Emulator
+							</Button>
+						</div>
+						<GitCard git={gitURL} />
+						<ContentCard git={gitURL} />
+					</Grid>
+					<Grid alignContent="center" container direction="column" xs={5}>
+						<div style={{}}>
+							<img src={phoneImg} style={{ maxHeight: '900px' }} />
+						</div>
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
 };
 
 Product.propTypes = {
-    classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
 };
 export default Product;
-
-
 
 //export default withStyles(classes)(Product);
