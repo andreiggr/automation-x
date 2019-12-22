8ebb558b5e920a0d59771ef4f1fe660b06a292a3
andreiggr <andrei.ggr@gmail.com>
2019-12-22 04:43:10 +0200

    refactor filters, active filter clearing, minor styling

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -3,44 +3,39 @@ import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Badge, Hidden, IconButton, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+import Categories from './Categories/Categories';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    boxShadow: 'none'
-  },
-  flexGrow: {
-    flexGrow: 1
-  },
-  signOutButton: {
-    marginLeft: theme.spacing(1)
-  }
+const useStyles = makeStyles((theme) => ({
+	root: {
+		boxShadow: 'none'
+	},
+	flexGrow: {
+		flexGrow: 1
+	},
+	signOutButton: {
+		marginLeft: theme.spacing(1)
+	}
 }));
 
-const Topbar = props => {
-  const { className, onSidebarOpen, ...rest } = props;
+const Topbar = (props) => {
+	const { className, onSidebarOpen, ...rest } = props;
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [notifications] = useState([]);
+	const [ notifications ] = useState([]);
 
-  return (
-    <AppBar
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <Toolbar>
-        <RouterLink to="/">
-          <img
-            alt="Logo"
-            src="/images/logos/logo--white.svg"
-          />
-        </RouterLink>
-        <div className={classes.flexGrow} />
-        {/* <Hidden mdDown>
+	return (
+		<AppBar {...rest} className={clsx(classes.root, className)}>
+			<Toolbar>
+				<RouterLink to="/">
+					<img alt="Logo" src="/images/logos/logo--white.svg" />
+				</RouterLink>
+				<div className={classes.flexGrow} />
+				{/* <Hidden mdDown>
           <IconButton color="inherit">
             <Badge
               badgeContent={notifications.length}
@@ -57,22 +52,19 @@ const Topbar = props => {
             <InputIcon />
           </IconButton>
         </Hidden> */}
-        <Hidden lgUp>
-          <IconButton
-            color="inherit"
-            onClick={onSidebarOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
-  );
+				<Hidden lgUp>
+					<IconButton color="inherit" onClick={onSidebarOpen}>
+						<MenuIcon />
+					</IconButton>
+				</Hidden>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 Topbar.propTypes = {
-  className: PropTypes.string,
-  onSidebarOpen: PropTypes.func
+	className: PropTypes.string,
+	onSidebarOpen: PropTypes.func
 };
 
 export default Topbar;
