37ba8181c27cc61b2e49d9a97d506e0f5c6e0ba7
andreiggr <andrei.ggr@gmail.com>
2020-01-26 21:45:10 +0200

    wworking on iframe

--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -34,14 +34,12 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
-const src =
-	'https://appetize.io/embed/zj3d2tfftfhjnv1f118yqq68cr?device=android&orientation=portrait&scale=65&xdocMsg=true&deviceColor=white&debug=false&screenOnly=false&autoplay=true';
-
 const Product = ({ selectedProduct }) => {
 	const classes = useStyles();
+	const src = `https://appetize.io/embed/${selectedProduct.publicKey}?device=android&orientation=portrait&scale=43&xdocMsg=true&deviceColor=white&debug=false&screenOnly=false&autoplay=true`;
 
-	const [run, setRun] = useState(false);
-	const [expired, setExpired] = useState(false);
+	const [ run, setRun ] = useState(false);
+	const [ expired, setExpired ] = useState(false);
 
 	const onStartApp = () => {
 		setRun(true);
@@ -56,29 +54,14 @@ const Product = ({ selectedProduct }) => {
 	};
 
 	return (
-		<Grid
-			className={classes.root}
-			container
-			direction="row"
-		>
-			<Grid
-				direction="column"
-				item
-			>
+		<Grid className={classes.root} container direction="row">
+			<Grid direction="column" item>
 				<div className={classes.contentBar}>
-					<Link
-						className={classes.backButton}
-						to="/products"
-					>
+					<Link className={classes.backButton} to="/products">
 						<ArrowBackIosIcon fontSize="small" />
 						<Typography className={classes.categoryTitle}>Go back</Typography>
 					</Link>
-					<Button
-						color="primary"
-						disabled={expired}
-						onClick={onStartApp}
-						variant="contained"
-					>
+					<Button color="primary" disabled={expired} onClick={onStartApp} variant="contained">
 						Run on Emulator
 					</Button>
 				</div>
@@ -89,22 +72,14 @@ const Product = ({ selectedProduct }) => {
 				/>
 				<ContentCard />
 			</Grid>
-			<Grid
-				direction="column"
-				item
-			>
-				<AppFrame
-					expired={expired}
-					runApp={run}
-					src={src}
-				/>
+			<Grid direction="column" item>
+				<AppFrame expired={expired} runApp={run} src={src} />
 			</Grid>
 		</Grid>
 	);
 };
 
-Product.propTypes = {
-};
+Product.propTypes = {};
 
 const mapStateToProps = (state) => {
 	return {
