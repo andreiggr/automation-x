7c394c0a198aceab74dd2822eff1ec8d686de861
Your Name <andrei.ggr@gmail.com>
2020-02-17 15:46:46 +0200

    some styling on product

--- src/views/Product/GitCard/GitCard.js
+++ src/views/Product/GitCard/GitCard.js
@@ -21,17 +21,36 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: 'space-between'
 	},
 	icon: {
-		margin: '8px',
 		cursor: 'pointer',
-		color: 'black'
+		color: '#66788A'
 	},
+	gitIcon: {
+		cursor: 'pointer',
+		color: 'black',
+		margin: "5px"
+	}
+	,
 	gitLink: {
 		display: 'flex',
 		alignItems: 'center',
 		cursor: 'pointer'
 	},
 	link: {
-		color: 'blue'
+		color: '#4183C4',
+		fontWeight: 'bold'
+	},
+	gitTag: {
+		marginLeft: '10px',
+		marginRight: '10px',
+		display: "flex",
+		alignItems: 'center',
+		justifyContent: "center",
+		backgroundColor: "#F5F5F5",
+		padding: "2px 9px",
+		borderRadius: '15px'
+	},
+	tagText: {
+		marginLeft: "3px", marginRight: "3px", color: '#66788A'
 	}
 }));
 
@@ -40,33 +59,56 @@ const GitCard = (props) => {
 
 	const classes = useStyles();
 
+	const gitLink = git.replace("https://github.com/", "")
+
 	return (
 		<Card
 			{...rest}
 			className={clsx(classes.root, className)}
 		>
 			<CardContent>
-				<Typography
-					align="left"
-					variant="h6"
-				>
-					GitHub
+				<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+					<Typography
+						align="left"
+						variant="h5"
+					>
+						GitHub
 				</Typography>
+					<div className={classes.gitTag}>
+						<VisibilityIcon className={classes.icon} />
+						<Typography
+							className={classes.tagText}
+							align="left"
+						>
+							Watch
+				</Typography>
+						<Typography
+							className={classes.tagText}
+							variant="h5">{watchers}</Typography>
+					</div>
+					<div className={classes.gitTag}>
+						<CallSplitIcon className={classes.icon} />
+						<Typography
+							className={classes.tagText}
+							align="left"
+						>
+							Fork
+				</Typography>
+						<Typography
+							className={classes.tagText}
+							variant="h5">{forks}</Typography>
+					</div>
+				</div>
 
 				<div className={classes.row}>
 					<div
 						className={classes.gitLink}
 						onClick={() => window.open(git, '_blank')}
 					>
-						<GitHubIcon className={classes.icon} />
-						<a className={classes.link}>{git}</a>
+						<GitHubIcon className={classes.gitIcon} />
+						<a className={classes.link}>{gitLink}</a>
 					</div>
-					<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-						<VisibilityIcon className={classes.icon} />
-						<Typography variant="h5">{watchers}</Typography>
-						<CallSplitIcon className={classes.icon} />
-						<Typography variant="h5">{forks}</Typography>
-					</div>
+
 				</div>
 			</CardContent>
 		</Card>
